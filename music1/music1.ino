5fed9b4c7ac8c9ab30410c2e2163c6a449a81d43
Tonikiller10000 <eaagtoni123.4@gmail.com>
2023-09-20 10:12:57 +0300

    Create Music With Arduino (Not Ready)

---
+++ music1/music1.ino
@@ -0,0 +1,33 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+/*
+tone();//pin,frequency,duration(optional)
+noTone();//pin
+
+
+Middle C: 261 Hz;
+c#: 277 Hz;
+D:  294 Hz;
+D#: 311 Hz;
+E:  330 Hz;
+F:  349 Hz;
+F#: 370 Hz;
+G:  392 Hz;
+G#: 415 Hz;
+A:  440 Hz;
+
+carefull at pins 3 and 11
+minim 31Hz ??
+one speaker at a time?
+*/
+
+
